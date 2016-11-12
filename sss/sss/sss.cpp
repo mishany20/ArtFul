6ec166bad9a9c768d8f32d4bcff4e9b3fbfae144
фывйцу <>
2016-11-12 11:14:40 +0800

    студент

---
+++ sss/sss/sss.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+class student
+{
+public:
+	char f,g; int n; int o[5]; float z;
+	char getname()
+	{
+		return f;
+	}
+	char getgrupa()
+	{
+		return g;
+	}
+	int getnomer()
+	{
+		return n;
+	}
+	student(char _f,char _g,int _n)
+	{
+		f=_f;
+		g=_g;
+		n=_n;
+	}
+}
+void main()
+{
+	char f;
+	student f(f,g,n)
+	
+
+
